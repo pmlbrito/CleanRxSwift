65784e67a138b51d54eadcd7bd7d52c7900cbcbb
Pedro Brito <pedroml.brito@gmail.com>
2016-07-02 12:55:22 +0100

    Removing all files in .gitignore

--- Pods/Target Support Files/QuickShotUtils/QuickShotUtils-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_QuickShotUtils : NSObject
-@end
-@implementation PodsDummy_QuickShotUtils
-@end
--- Pods/Target Support Files/QuickShotUtils/QuickShotUtils-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double QuickShotUtilsVersionNumber;
-FOUNDATION_EXPORT const unsigned char QuickShotUtilsVersionString[];
-
